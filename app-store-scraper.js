8d6092edb44087d7b40d8046bd0edaa7b039c1f9
Darcy Parks <darcy.parks@liu.se>
2022-10-07 10:49:02 +0200

    Changed input file from apps.json to appdatabase.csv

--- app-store-scraper.js
+++ app-store-scraper.js
@@ -4,49 +4,103 @@ const fs = require('fs').promises;
 const fetch = require('node-fetch');
 
 const CONFIG = require('./config.json');
-const APPCONFIG = require('./apps.json');
+
+const csv = require('@fast-csv/parse');
+
+const locales = [
+	{
+		"name": "Sweden-Swedish",
+		"gplay_lang": "sv",
+		"gplay_country": "sv",
+		"apple_lang": "sv",
+		"apple_country": "se"
+	},
+	{
+		"name": "UK-English",
+		"gplay_lang": "en_GB",
+		"gplay_country": "UK",
+		"apple_lang": "en-gb",
+		"apple_country": "gb"
+	}
+];
 
 async function scrape_app (store, app_details) {
-	store.app(app_details).
-		then((response) => {
-			console.log(app_details.name);
-			var foldername = CONFIG.root_folder.
-				concat(app_details.name).
-				concat('(').
-				concat(app_details.country).
-				concat('.').
-				concat(app_details.lang).
-				concat(')/').
-				concat(new Date().toISOString());
-			fs.mkdir(foldername, { recursive: true }).
-				then ( () => {
-					fs.writeFile(foldername.concat('/data.json'), JSON.stringify(response, null, 2));
-				}).
-				then ( () => {
-					let screenshotnum = 0;
-					for (screenshot of response.screenshots) {
-						screenshotnum = screenshotnum + 1;
-						let filename = foldername.concat('/screenshot').concat(screenshotnum).concat('.png');
-						fetch(screenshot).
-							then( res => res.arrayBuffer()).
-							then( b => fs.writeFile(filename, Buffer.from(b)));
-					}
-				});
-		}).
-		catch(error => {
-			console.log('Error:', app_details.name);
-			console.log(error);
+	store.app(app_details)
+	.then((response) => {
+		//console.log(app_details.name);
+		var foldername = CONFIG.root_folder.
+			concat(app_details.name).
+			concat('(').
+			concat(app_details.country).
+			concat('.').
+			concat(app_details.lang).
+			concat(')/').
+			concat(new Date().toISOString());
+		fs.mkdir(foldername, { recursive: true })
+		.then ( () => {
+			fs.writeFile(foldername.concat('/data.json'), JSON.stringify(response, null, 2));
+		})
+		.then ( () => {
+			let screenshotnum = 0;
+			for (screenshot of response.screenshots) {
+				screenshotnum = screenshotnum + 1;
+				let filename = foldername.concat('/screenshot').concat(screenshotnum).concat('.png');
+				fetch(screenshot).
+					then( res => res.arrayBuffer()).
+					then( b => fs.writeFile(filename, Buffer.from(b)));
+			}
 		});
+		console.log('Success:', app_details.name, app_details.lang, app_details.country);
+	})
+	.catch(error => {
+		console.error('Error:', app_details.name, app_details.lang, app_details.country, error.message);
+	});
 }
 
-for (app of APPCONFIG.gplay_apps) {
-	for (locale of app.locales) {
-		scrape_app(gplay, {name: app.name, appId: app.text_id, lang: locale.lang, country: locale.country});
+function process_row (row) {
+	// Loop through the languages
+	// Do both app stores
+	console.log("Reading CSV:", row['App Name']);
+	for (locale of locales) {
+		if (row[locale.name] === "Yes") {
+			if (row['Google ID'] === "") {
+				console.log("Skipping:",
+					row['App Name'].concat(' Android'),
+					locale['gplay_lang'],
+					locale['gplay_country']
+				);
+			} else {
+				scrape_app(gplay, {
+					"name": row['App Name'].concat(" Android"),
+					"appId": row['Google ID'],
+					"lang": locale['gplay_lang'], 
+					"country": locale['gplay_country']
+				});
+			}
+
+			if (row['Apple ID'] === "") {
+				console.log("Skipping:",
+					row['App Name'].concat(' iOS'),
+					locale['apple_lang'],
+					locale['apple_country']
+				);
+			} else {
+				scrape_app(apple, {
+					"name": row['App Name'].concat(" iOS"),
+					"id": row['Apple ID'],
+					"lang": locale['apple_lang'], 
+					"country": locale['apple_country']
+				});
+			}
+
+		}
 	}
+	
 }
 
-for (app of APPCONFIG.apple_apps) {
-	for (locale of app.locales) {
-		scrape_app(apple, {name: app.name, id: app.numerical_id, lang: locale.lang, country: locale.country});
-	}
-}
+csv.parseFile('appdatabase.csv', { headers: true})
+	.on('error', error => console.error(error))
+	//.on('data', row => console.log(`ROW=${JSON.stringify(row)}`))
+	//.on('data', row => console.log(row['Category']))
+	.on('data', row => process_row(row))
+	.on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
